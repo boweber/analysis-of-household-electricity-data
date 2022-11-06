5c1a3038754786d0191e6df2f8db8d0c47805a14
Bo Weber <git@boweber.de>
2022-11-06 10:17:16 +0100

    Added import_ruhrgebiet_data, Removed shift_dates, Added group_by_day_and_night

--- ImportData.R
+++ ImportData.R
@@ -1,17 +1,19 @@
-library("tibble")
 ## a function to import the data
 ##
 ## on_import can be a function that is called on each dataset.
-## -> this can be used to modify the dataset before adding the dataset to the list
+## -> this can be used to modify the dataset before
+## adding the dataset to the list
 ##
-## file_numbers can be a vector of file numbers or a number and determines which files are imported
+## file_numbers can be a vector of file numbers or a
+## number and determines which files are imported
 ## if file_numbers is null, all files are imported
 ##
-## file_path specifies the path to the files, by default the files are expected to be in
+## file_path specifies the path to the files, by
+## default the files are expected to be in
 ## a folder called "OHNE PV" in the current working directory
 ##
 ## returns a list of datasets
-import_data <- function(on_import = NULL, file_numbers = NULL, file_path = "OHNE\ PV/") {
+import_data <- function(file_numbers = NULL, file_path = "OHNE\ PV/") {
     ## A helper function to get the file path of a specific number
     ## e.g.: get_file_path(3) returns "OHNE PV/SFH3.csv"
     file_directory <- function(number) {
@@ -32,17 +34,71 @@ import_data <- function(on_import = NULL, file_numbers = NULL, file_path = "OHNE
         }
         if (file.exists(file_directory(number))) {
             file_data <- readr::read_csv(file_directory(number))
-            file_data <- file_data %>% dplyr::rename("TEMPERATURE.TOTAL" = "TEMPERATURE:TOTAL")
-            file_data$household <- rep(number, nrow(file_data))
-            if (!is.null(on_import)) {
-                file_data <- on_import(file_data)
-            }
+            file_data <- dplyr::rename(
+                file_data,
+                "TEMPERATURE.TOTAL" = "TEMPERATURE:TOTAL"
+            )
+            file_data$household <- as.factor(rep(number, nrow(file_data)))
             if (is.null(data)) {
                 data <- file_data
             } else {
-                data <- data %>% tibble::add_row(file_data)
+                data <- tibble::add_row(data, file_data)
             }
         }
     }
     return(data)
 }
+
+import_ruhrgebiet_data <- function(cities = NULL) {
+    last_index <- stringr::str_length("Ruhrgebiet - Daten//")
+
+    data <- NULL
+    for (folder_name in list.dirs(path = "Ruhrgebiet - Daten")) {
+        city <- substring(
+            folder_name,
+            last_index,
+            stringr::str_length(folder_name)
+        )
+        if (!is.null(cities) && !(city %in% cities)) {
+            next
+        }
+        if (stringr::str_length(city) > 0) {
+            file_path <- paste(folder_name, "/", city, ".csv", sep = "")
+
+            file_number_of_city <- 1
+            for (file_name in list.files(path = folder_name)) {
+                if (grepl(".csv", file_name)) {
+                    file_path <- paste(folder_name, "/", file_name, sep = "")
+                    if (file.exists(file_path)) {
+                        file_data <- readr::read_csv(
+                            file_path,
+                            col_types = list(
+                                readr::col_datetime(),
+                                readr::col_double()
+                            )
+                        )
+                        file_data <- dplyr::rename(
+                            file_data,
+                            "index" = "Zeitstempel"
+                        )
+                        household <- paste(city, file_number_of_city, sep = "_")
+                        file_data$household <- as.factor(
+                            rep(household, nrow(file_data))
+                        )
+                        na_indices <- which(is.na(file_data$index))
+                        file_data <- file_data[-na_indices, ]
+                        if (is.null(data)) {
+                            data <- file_data
+                        } else {
+                            data <- tibble::add_row(data, file_data)
+                        }
+                        file_number_of_city <- file_number_of_city + 1
+                    }
+                }
+            }
+        }
+    }
+    return(data)
+}
+
+test <- import_ruhrgebiet_data(cities = c("Dortmund"))
--- ModifyData.R
+++ ModifyData.R
@@ -1,113 +1,123 @@
-library(lubridate) ## required for %--%
-library(tibble) ##  required for %>%
-
-shift_dates <- function(dataset,
-                        by_minutes,
-                        shift_data_column_name = c("PUMPE_TOT"),
-                        date_column_name = "index") {
-    if (by_minutes == 0 || nrow(dataset) == 0) {
-        return(dataset)
-    }
+insert_missing_data <- function(household_dataset, by_minutes = 15) {
     if ((by_minutes %% 15) != 0) {
         stop("by_minutes must be a multiple of 15")
     }
     if (by_minutes < 0) {
         stop("by_minutes must be positive")
     }
-    if (!lubridate::is.POSIXt(dataset[[date_column_name]])) {
-        stop(paste(date_column_name, " must be of type POSIXt"))
-    }
-    number_of_missing_data_points <- 0
-    determine_index <- function(index, next_index) {
-        if (nrow(dataset) < next_index) {
-            return(NULL)
-        } else if (index == next_index) {
-            return(determine_index(index, next_index + 1))
-        } else if (nrow(dataset) < index) {
-            stop("Invalid index")
-        }
-        current_date <- dataset[index, ][[date_column_name]]
-        next_date <- dataset[next_index, ][[date_column_name]]
-        ## the elpased minutes between the two dates
-        time_interval <- lubridate::as.duration(current_date %--% next_date) / 60
-        if ((time_interval %% 15) != 0) {
-            stop("time_interval must be a multiple of 15")
-        }
-        if (time_interval == by_minutes) {
-            return(next_index)
-        } else if (time_interval > by_minutes) {
-            number_of_missing_data_points <- number_of_missing_data_points + 1
-            return(next_index)
-        } else {
-            return(determine_index(index, next_index + 1))
-        }
-    }
+    first_date <- household_dataset[1, ]$index
+    last_date <- household_dataset[nrow(household_dataset), ]$index
 
-    # construct a new empty dataset with the same column names as the dataset
-    shifted_data <- dataset[1, ]
-    shifted_data <- shifted_data[-1, ]
-    time_index <- 1
-    for (data_index in seq_len(nrow(dataset))) {
-        if (data_index %% 1000 == 0) {
-            print(paste("Currently at index", data_index))
-        }
-        time_index <- determine_index(data_index, time_index)
-        if (is.null(time_index)) {
-            break
-        } else {
-            new_row <- dataset[data_index, ]
-            if (is.vector(shift_data_column_name)) {
-                for (column_name in shift_data_column_name) {
-                    new_row[[column_name]] <- dataset[time_index, ][[column_name]]
-                }
-            } else {
-                new_row[[shift_data_column_name]] <- dataset[time_index, ][[shift_data_column_name]]
-            }
-            shifted_data <- shifted_data %>% tibble::add_row(tibble::tibble_row(new_row))
-        }
-    }
-    if (number_of_missing_data_points > 0) {
-        warning(paste(number_of_missing_data_points, " data points were missing"))
-    }
-    return(shifted_data)
+    completed <- tibble::tibble(
+        index = seq(first_date, last_date, by = paste(by_minutes, "min"))
+    )
+    completed$household <- as.factor(
+        rep(household_dataset$household[1], nrow(completed))
+        )
+    return(merge(household_dataset, completed, all.y = TRUE))
 }
 
-insert_missing_data <- function(dataset, by_minutes = 30) {
-    if ((by_minutes %% 15) != 0) {
-        stop("by_minutes must be a multiple of 15")
-    }
-    if (by_minutes < 0) {
-        stop("by_minutes must be positive")
-    }
-    first_date <- dataset[1, ]$index
-    last_date <- dataset[nrow(dataset), ]$index
-
-    completed <- tibble(index = seq(first_date, last_date, by = paste(by_minutes, "min")))
-    completed$household <- rep(dataset$household[1], nrow(completed))
-    return(merge(dataset, completed, all.y = TRUE))
-}
-
-interpolate_missing_data <- function(dataset, column_name = "TEMPERATURE.TOTAL", interpolate = base::mean) {
+interpolate_missing_data <- function(household_dataset,
+                                     column_name = "TEMPERATURE.TOTAL",
+                                     interpolate = base::mean) {
     index <- 1
 
-    while (any(is.na(dataset[[column_name]]))) {
-        missing_data_indices <- which(is.na(dataset[[column_name]]))
-        before_after_values <- matrix(c(dataset[[column_name]][missing_data_indices - index], dataset[[column_name]][missing_data_indices + index]), ncol = 2)
-        interpolated_data <- apply(before_after_values, 1, interpolate, na.rm = TRUE)
-        dataset[[column_name]][missing_data_indices] <- interpolated_data
+    while (any(is.na(household_dataset[[column_name]]))) {
+        missing_data_indices <- which(is.na(household_dataset[[column_name]]))
+        before_after_values <- matrix(c(
+            household_dataset[[column_name]][missing_data_indices - index],
+            household_dataset[[column_name]][missing_data_indices + index]
+        ), ncol = 2)
+        interpolated_data <- apply(
+            before_after_values, 1, interpolate,
+            na.rm = TRUE
+        )
+        household_dataset[[column_name]][missing_data_indices] <- interpolated_data
         index <- index + 1
         if (index > 4) {
             warning("Stopped interpolation after 4 iterations")
             break
         }
     }
-    return(dataset)
+    return(household_dataset)
 }
 
-remove_missing_data <- function(dataset, column_name = "TEMPERATURE.TOTAL", threshold = 0.01) {
+remove_missing_data <- function(dataset,
+                                column_name = "TEMPERATURE.TOTAL",
+                                threshold = 0.01) {
     na_indices <- which(is.na(dataset[[column_name]]))
     if (length(na_indices) / nrow(dataset) > threshold) {
-        warning(paste("More than ", threshold * 100, "% of the data is missing"))
+        warning(paste(
+            "More than ", threshold * 100, "% of the data is missing"
+        ))
     }
     return(dataset[-which(is.na(dataset[[column_name]])), ])
+}
+
+group_by_day_and_night <- function(dataset) {
+    all_households <- unique(dataset$household)
+
+    grouped_dataset <- tibble::tibble(
+        PUMPE_TOT = numeric(),
+        TEMPERATURE.TOTAL = numeric(),
+        household = factor(),
+        isDay = factor()
+    )
+    for (household in all_households) {
+        household_data <- dataset[which(dataset$household == household), ]
+        household_data <- insert_missing_data(household_data)
+        if (nrow(household_data) == 0) {
+            next
+        }
+        hours <- as.numeric(format(household_data$index, "%H"))
+        household_data$isDay <- as.factor(
+            ifelse(hours >= 8 & hours < 20, "day", "night")
+        )
+
+        first_day_night_shift_index <- 1
+        first_is_day <- household_data$isDay[1]
+        while (first_is_day == household_data$isDay[first_day_night_shift_index]) {
+            first_day_night_shift_index <- first_day_night_shift_index + 1
+        }
+
+        last_day_night_shift_index <- nrow(household_data)
+        last_is_day <- household_data$isDay[nrow(household_data)]
+        while (last_is_day == household_data$isDay[last_day_night_shift_index]) {
+            last_day_night_shift_index <- last_day_night_shift_index - 1
+        }
+
+        number_of_day_night_changes <- (
+            last_day_night_shift_index - first_day_night_shift_index + 1
+        ) / (12 * 4)
+        if (number_of_day_night_changes %% 1 != 0) {
+            stop(paste(
+                "number_of_day_night_changes must be an integer",
+                number_of_day_night_changes,
+                "current household:",
+                household
+            ))
+        }
+
+        first_group <- rep(1, (first_day_night_shift_index - 1))
+        middle_groups <- rep(2:(number_of_day_night_changes + 1), each = 12 * 4)
+        last_group <- rep(
+            number_of_day_night_changes + 2,
+            (nrow(household_data) - last_day_night_shift_index)
+        )
+        household_data$group <- c(first_group, middle_groups, last_group)
+        grouped_household_data <- dplyr::summarise(
+            dplyr::group_by(household_data, group),
+            PUMPE_TOT = mean(PUMPE_TOT, trim = 0.05, na.rm = TRUE),
+            isDay = isDay[1],
+            TEMPERATURE.TOTAL = mean(TEMPERATURE.TOTAL, na.rm = TRUE)
+        )
+        grouped_household_data <- dplyr::select(grouped_household_data, -group)
+        grouped_household_data$household <- as.factor(rep(
+            household, nrow(grouped_household_data)
+        ))
+        grouped_dataset <- tibble::add_row(
+            grouped_dataset, grouped_household_data
+        )
+    }
+    return(grouped_dataset)
 }
